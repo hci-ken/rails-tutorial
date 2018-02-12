f2f00d4b56286105449d7c9b9ba67a59116ec093
hci-ken <honken@complex.ist.hokudai.ac.jp>
2018-02-12 21:57:50 +0900

    Implement skill removal function

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,5 +1,6 @@
 class SkillsController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
+    before_action :correct_user,   only: :destroy
     def create
         @user = User.find(params[:skill][:user_id])
         @skill = @user.skills.build(skill_params)
@@ -13,11 +14,20 @@ class SkillsController < ApplicationController
     end
 
     def destroy
+        #@skill.destroy
+        Skill.find(params[:id]).destroy
+        flash[:success] = "Skill deleated"
+        redirect_to request.referrer || root_url
     end
 
     private
 
         def skill_params
             params.require(:skill).permit(:name,:user_id)
+        end
+
+        def correct_user
+            @skill = current_user.skills.find_by(id: params[:id])
+            redirect_to root_url if @skill =nil?
         end
 end
