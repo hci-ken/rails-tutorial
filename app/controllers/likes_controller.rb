f7742ea24c610918dbd6f3af6ebbc6c103aef5cd
hci-ken <honken@complex.ist.hokudai.ac.jp>
2018-02-13 12:41:15 +0900

    Add likes counting function

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,6 +5,7 @@ class LikesController < ApplicationController
     @skill = Skill.find(params[:skill_id]) 
     unless @skill.plus?(current_user)
       @skill.plus(current_user)
+      @skill.reload
       respond_to do |format|
         format.html { redirect_to request.referrer || root_url}
         format.js
@@ -13,9 +14,11 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    @skill = Like.find(params[:id].micropost)
+    @skill = Like.find(params[:id]).skill
     if @skill.plus?(current_user)
       @skill.minus(current_user)
+      redirect_to @user
+      @skill.reload
       respond_to do |format|
         format.html { redirect_to request.referrer || root_url }
         format.js
